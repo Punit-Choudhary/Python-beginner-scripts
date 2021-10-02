cf50bbcb0151661b00e8450a9eb78990a253aeac
Arpit Soni <arpitsoni362@gmail.com>
2021-10-02 17:54:38 +0530

    spelling checker in python [formatted]

--- spellingChecker.py
+++ spellingChecker.py
@@ -1,10 +1,7 @@
-# pip install textblob
-
 from textblob import TextBlob 
   
 a = "comuter progrming is gret"
 print("original text: "+str(a)) 
   
 b = TextBlob(a) 
-
-print("corrected text: "+str(b.correct()))    
+print("corrected text: "+str(b.correct()))
