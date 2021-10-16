b49431082c65098d1d59af499636e4487a17a8c4
Burhan Chhotaudepur <65703940+burhanxd@users.noreply.github.com>
2021-10-16 17:36:39 +0530

    Update direnum.py

--- direnum.py
+++ direnum.py
@@ -8,7 +8,6 @@ dirs = sublist.splitlines()
 for dir in dirs:
     direnum = f"http://{sys.argv[1]}/{dir}"
     r = requests.get(direnum)
-    print(r.url)
     if r.status_code == 404:
         pass
     else:
