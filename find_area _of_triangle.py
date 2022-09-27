47b08100924761a3eabd00d6296feae4aa4620bf
Punit Choudhary <itspunitchoudhary@gmail.com>
2022-09-27 23:02:37 +0530

    Sorting

--- find_area _of_triangle.py
+++
@@ -1,9 +0,0 @@
-a = float(input('Enter first side: '))  
-b = float(input('Enter second side: '))  
-c = float(input('Enter third side: '))  
-  
-s = (a + b + c) / 2  
-  
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
-print('The area of the triangle is:', area)   
-
