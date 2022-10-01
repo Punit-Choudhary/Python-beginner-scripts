144e882762a3f9520e747d7ae60570bf6e09bcea
Yashwanth <yashwanth5836@gmail.com>
2022-10-01 20:12:19 +0530

    Added code for numeric pattern 2

---
+++ Patterns/Numeric Patterns/Code/Numeric_Pattern_2/main.py
@@ -0,0 +1,15 @@
+# This function is used to generate our required pattern.
+# It takes an integer n as an input.
+# There are two nested for loops in it.
+# Outer loop is used to handle the number of rows and the Inner nested loop is used to handle the number of columns.
+# Think of it in this way, using the inner loop, we print 1 2 3 4 5. Then we use outer loop to repeat this.
+def PatternGenerator(n):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            print(j, end=" ")
+# Inside the loops, we use print() to print the value j for each iteration of the loop.
+# j starts from 1 and as the loop goes on, it increments its value and prints other numbers
+        print('\n')
+
+
+PatternGenerator(5)
---
+++ Patterns/Numeric Patterns/Code/Numeric_Pattern_2/readme.md
@@ -0,0 +1,15 @@
+# Numeric Pattern 1
+
+**Pattern Image:**
+
+Eg:
+
+![image](../../img/numericpattern2.PNG)
+
+**Contributor:** [Avantsa Raja Yashwanth](https://github.com/R-Yash)
+
+:star2: Star it :fork_and_knife:Fork it :handshake: Contribute to it!
+
+Discord server - http://pragmaticprogrammer.in/discord
+
+Happy Coding :purple_heart:
