21998649e4fda84a8798c924ae504928a5c39bbc
Hassaan Anwar <hassaananwar12345@gmail.com>
2022-10-01 15:52:32 +0500

    Added Changes For Flake8

--- Math/area_of_triangle.py
+++ Math/area_of_triangle.py
@@ -3,7 +3,7 @@ base = float(input('Enter The Base Of The Triangle: '))
 height = float(input('Enter The Height Of The Trianlge: '))
 
 # Calculating The Area Of The Triangle
-area = (base * height)/2 
+area = (base * height)/2
 
 # Ouputting The Area
 print('The Area Of The Triangle Is:', area)
