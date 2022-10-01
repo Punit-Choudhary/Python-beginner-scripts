834e6f3a08f9c1c0a802312c185e8a6aa2caba62
Hassaan Anwar <hassaananwar12345@gmail.com>
2022-10-01 15:38:29 +0500

    Updated The Code To Work On 2 Parameters

---
+++ Math/area_of_triangle.py
@@ -0,0 +1,9 @@
+# Taking Base And Height Inputs
+base = float(input('Enter The Base Of The Triangle: '))
+height = float(input('Enter The Height Of The Trianlge: '))
+
+# Calculating The Area Of The Triangle
+area = (base * height)/2 
+
+# Ouputting The Area
+print('The Area Of The Triangle Is:', area)
