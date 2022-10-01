231e09a4ab5b0db0238c3fec4359ad2ff7342abe
Harishrirajendra <97623096+Harishrirajendra@users.noreply.github.com>
2022-10-01 23:05:34 +0530

    Update symbol_pattern_126.py

--- Patterns/Symbol Patterns/code/Symbol_Pattern_126.py/symbol_pattern_126.py
+++ Patterns/Symbol Patterns/code/Symbol_Pattern_126.py/symbol_pattern_126.py
@@ -1,10 +1,10 @@
-#Created a nested loop of 7*7
-#printed '*' character at row 7 coloumn 7 and diagonal
-#else printed space and later added new line after each row iteration
+# Created a nested loop of 7*7
+# printed '*' character at row 7 coloumn 7 and diagonal
+# else printed space and later added new line after each row iteration
 for i in range(8):
     for j in range(8):
-        if(i==7 or j==7 or i==j):
-            print("*",end=' ')
+        if (i == 7 or j == 7 or i == j):
+            print("*", end=' ')
         else:
-            print(' ',end=' ')
+            print(' ', end=' ')
     print()
