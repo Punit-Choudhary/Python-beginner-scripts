6808b37a787f06d2771bd112851df8d99cefc205
TrainedPro <90309454+TrainedPro@users.noreply.github.com>
2022-10-02 23:20:45 +0500

    "Added changes to make code more readible"

--- Math/circumference_of_Circle.py
+++ Math/circumference_of_Circle.py
@@ -3,7 +3,7 @@ def Circumference():
     radius = float(input('Enter Radius Of Circle: '))
 
     # The formula to calculate circumference
-    circumference = 2*3.14159*radius
+    circumference = 2 * 3.14159 * radius
 
     print('The Circumference Of The Circle Is:', circumference)
 
