97eee5b15c0b13334290d8e6fe01770858b452e7
Harishrirajendra <97623096+Harishrirajendra@users.noreply.github.com>
2022-10-02 19:33:39 +0530

    Update Symbol_Pattern_184.py

--- Patterns/Symbol Patterns/code/Symbol_Pattern_184.py/Symbol_Pattern_184.py
+++ Patterns/Symbol Patterns/code/Symbol_Pattern_184.py/Symbol_Pattern_184.py
@@ -5,7 +5,11 @@ x = int(input('Enter Number :'))
 # Enter dimensions of square box for which we want to draw pattern
 for i in range(x):  # iterations for rows
     for j in range(x):  # iterations for column
-        if (((i == 0 or i == x-1) and j < x-1) or j == 0 or (j == x-1 and 0 < i < x-1)):
+        if (((i == 0 or i == x-1) and j < x-1)):
+            print("*", end=' ')
+        elif (j == x-1 and 0 < i < x-1):
+            print("*", end=' ')
+        elif j == 0:
             print("*", end=' ')
         else:
             print(" ", end=' ')
