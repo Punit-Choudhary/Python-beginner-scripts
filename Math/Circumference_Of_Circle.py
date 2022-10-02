43e44d9e1255c360ed29ad26f6b488060cd18e81
Hassaan Anwar <hassaananwar12345@gmail.com>
2022-10-02 20:26:22 +0500

    Added Math File

---
+++ Math/Circumference_Of_Circle.py
@@ -0,0 +1,11 @@
+# Defining the function to calculate the circumference of a circle
+def Circumference():
+    radius = float(input('Enter Radius Of Circle: '))
+
+    # The formula to calculate circumference
+    circumference = 2*3.14159*radius
+
+    print('The Circumference Of The Circle Is:', circumference)
+
+
+Circumference()
