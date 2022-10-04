caf3c2b5daa9b9bacbaa2e91b49a994fa01487b0
saimaheshtaduri <saimaheshtaduri6@gmail.com>
2022-10-04 22:47:28 +0530

    updated code as per screenshot

--- Patterns/Pyramid_Patterns/Code/pyramid_pattern_20/pyramid_pattern20.py
+++ Patterns/Pyramid_Patterns/Code/pyramid_pattern_20/pyramid_pattern20.py
@@ -1,24 +1,29 @@
-#The below function gives the pattern similar to the pyramid pattern 20
-#Here there is a triangle with 1 element in the first row, 3 elements in second, 5 in third and so on
-#all the elements are in increasing order by 1
-
-#I took 3 variables, i.e. spaces, number, row
-#first, I print the spaces, then loop over row times and print numbers and then print spaces.
-#refer picture for clear understanding. After every outer iteration, decrease spaces by 1 and increase row by 2(as every row has odd elements)
-#thus, the pattern can be solved using python
+# The below function gives the pattern similar to the pyramid pattern 20
+# Here there is a triangle with 1 element in the first row,
+# 3 elements in second, 5 in third and so on
+# all the elements are in increasing order by 1
+# I took 3 variables, i.e. spaces, number, row
+# first, I print the spaces, then loop over row times and
+# print numbers and then print spaces.
+# refer picture for clear understanding.
+# After every outer iteration, decrease spaces by 1 and increase
+# row by 2(as every row has odd elements)
+# thus, the pattern can be solved using python
 
 def printPyramid(n):
-    spaces = n-1
+    spaces = 3*(n-1)
     num = 1
     row = 1
     while spaces >= 0:
-        print(' '*spaces, end=" ")
-        for i in range(row):
+        print(" " * spaces, end="")
+        for _ in range(row):
+            if (num < 10):
+                print(" ", end="")
             print(num, end=" ")
             num += 1
-        print(' '*spaces, end=" ")
+        print(" "*spaces, end="")
         print()
-        spaces -= 1
+        spaces -= 3
         row += 2
 
 
