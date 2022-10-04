8dafa7704c48dc3b7d73866f22dd6e6cd7b65190
codingis4noobs2 <87560178+codingis4noobs2@users.noreply.github.com>
2022-10-05 01:04:46 +0530

    Update visualizer.py

--- Projects/Visualize3D_Cube/visualizer.py
+++ Projects/Visualize3D_Cube/visualizer.py
@@ -1,6 +1,7 @@
 import pygame
 import ice
 
+
 pygame.init()
 
 
@@ -14,28 +15,25 @@ pygame.display.set_caption('Ice3D Use WASD to turn cube')
 screen.fill("white")
 
 
-cube = ice.Cube((screen_x/2,screen_y/2),50)
+cube = ice.Cube((screen_x/2, screen_y/2), 50)
 
 
 def main():
-  clock = pygame.time.Clock()
-  
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        return
-    
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
     keys = pygame.key.get_pressed()
-    
     if keys[pygame.K_a]:
-      cube.rotate(0,-3,0)
+        cube.rotate(0, -3, 0)
     if keys[pygame.K_d]:
-      cube.rotate(0,3,0)
+        cube.rotate(0, 3, 0)
     if keys[pygame.K_w]:
-      cube.rotate(3,0,0)
+        cube.rotate(3, 0, 0)
     if keys[pygame.K_s]:
-      cube.rotate(-3,0,0)
+        cube.rotate(-3, 0, 0)
     '''if keys[pygame.K_LEFT]: # movement is kinda lame rn lmao
       cube.move(-3,0)
     if keys[pygame.K_RIGHT]:
@@ -44,11 +42,8 @@ def main():
       cube.move(0,-3)
     if keys[pygame.K_DOWN]:
       cube.move(0,3)'''
-    
     screen.fill("white")
-    
     cube.render(screen)
-    
     clock.tick(30)
     pygame.display.update()
 
