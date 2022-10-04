7d6c8b6274e646694905fae76ca120fc1c737bab
nirban07 <nirban012@gmail.com>
2022-10-04 20:38:10 +0530

    flake8 lintng passed

--- Patterns/Symbol_Patterns/code/Symbol_Pattern_16/symbol_pattern16.py
+++ Patterns/Symbol_Patterns/code/Symbol_Pattern_16/symbol_pattern16.py
@@ -1,5 +1,4 @@
 # this code will give us the required pattern number 16
-# img link: https://github.com/Punit-Choudhary/Python-beginner-scripts/blob/main/Patterns/Symbol_Patterns/img/16.PNG
 
 for i in range(5):
     print('* '*(5-i-1) + "{} ".format(i+1)*(i+1))
