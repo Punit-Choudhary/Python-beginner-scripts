223d7a21b54fab51822782ccc22111972baebf88
Daksh Jain <dakshjain@Daksh-jains-MacBook-Pro.local>
2022-10-06 18:33:11 +0530

    add pyramid pattern 2 code

---
+++ Patterns/Pyramid_Patterns/Code/pyramid_pattern_2/pyramid_pattern2.py
@@ -0,0 +1,7 @@
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(i, -1, -1):
+        print(i+1, end=" ")
+    print()
