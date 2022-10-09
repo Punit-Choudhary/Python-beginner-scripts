75f0ad29fe23e5fc3182e87b41fee3cabef69661
nirban07 <nirban012@gmail.com>
2022-10-09 14:17:17 +0530

    linting checked

--- Patterns/Symbol_Patterns/code/Symbol_Pattern_13/symbol_pattern13.py
+++ Patterns/Symbol_Patterns/code/Symbol_Pattern_13/symbol_pattern13.py
@@ -1,4 +1,4 @@
-# this code will genrate pattern number #13 
-size  = 5
+# this code will genrate pattern number #13
+size = 5
 for i in range(size):
-	print(" ".join([str(x+1) for x in range(i+1)]) + ' *'*(size-i-1))
+    print(" ".join([str(x+1) for x in range(i+1)]) + ' *'*(size-i-1))
