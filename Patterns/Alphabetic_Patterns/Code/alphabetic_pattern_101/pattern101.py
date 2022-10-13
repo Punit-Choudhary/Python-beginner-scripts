20a0f9ebbc7b7e5fadf43b7dd627a91265e622c4
Chandrahas <chandugumidelli@gmail.com>
2022-10-13 19:28:13 +0530

    Alphabetic pattern 101 added

---
+++ Patterns/Alphabetic_Patterns/Code/alphabetic_pattern_101/pattern101.py
@@ -0,0 +1,10 @@
+# Alpahbetic Pattern 101
+
+for i in range(5):
+    print(' ' * (4 - i), end='')
+    for x in range(2 * i + 1):
+        if x == 0 or x == 2 * i:
+            print(chr(i + 65), end='')
+        else:
+            print(' ', end='')
+    print()
