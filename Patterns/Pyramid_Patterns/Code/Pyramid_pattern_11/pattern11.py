4bfc2f7c9f7502e48fa9a6494fcaf8737a2abc22
Punit Choudhary <65050160+Punit-Choudhary@users.noreply.github.com>
2022-10-13 18:21:48 +0530

    patch lint

--- Patterns/Pyramid_Patterns/Code/Pyramid_pattern_11/pattern11.py
+++ Patterns/Pyramid_Patterns/Code/Pyramid_pattern_11/pattern11.py
@@ -1,10 +1,9 @@
-#pyramid pattern 11
-#outer for loop to print the number of rows
-#inner for loop to print characters according to 
-#the given pattern
-#
+# Pyramid Pattern 11
+# outer for loop to print the number of rows
+# inner for loop to print characters according to
+# the given pattern
 for i in range(5):
-    print(' '*i,end='')
-    for x in range(5-i):
-        print(chr(65+x),end=' ')
+    print(' ' * i, end='')
+    for x in range(5 - i):
+        print(chr(65 + x), end=' ')
     print()
