86f8d2e917590730eab8efd0e022bee8db1ae453
Vedant Maske <vedantrmaske03@gmail.com>
2022-10-13 17:49:20 +0530

    Fuck me

---
+++ Math/sum_of_n_natural_numbers.py
@@ -0,0 +1,9 @@
+#this function is used to calculate sum of 
+# n natural numbers.
+# we have defined a function here named sum 
+# which takes n as a argument  
+def sum(n):
+#formula to calculate sum of n natural numbers
+    return n * (n+1) /2
+
+print(sum(7))
