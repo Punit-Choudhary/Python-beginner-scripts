49f71cb2aac460c70d7d30ea6e81505fd14f3719
Punit Choudhary <65050160+Punit-Choudhary@users.noreply.github.com>
2022-10-18 16:43:58 +0530

    Update volume_hollow_hemisphere.py

--- Math/volume_hollow_hemisphere.py
+++ Math/volume_hollow_hemisphere.py
@@ -1,6 +1,6 @@
 from math import pi
 
-oradius = int(input('Enter Outer Radius: '))
-iradius = int(input('Enter Inner Radius: '))
-volume = (2/3) * pi * (oradius**3-iradius**3)
+outer_radius = int(input('Enter Outer Radius: '))
+inner_radius = int(input('Enter Inner Radius: '))
+volume = (2/3) * pi * (outer_radius**3 - inner_radius**3)
 print("Volume of a Hollow Hemisphere is: {}".format(volume))
