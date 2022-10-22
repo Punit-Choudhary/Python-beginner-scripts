bf6c1ce6d8f4f6504b86dac22fcf2026750003a0
Punit Choudhary <65050160+Punit-Choudhary@users.noreply.github.com>
2022-10-23 00:50:42 +0530

    fix linting issues

--- Math/sqauresum.py
+++ Math/sqauresum.py
@@ -1,4 +1,6 @@
-def sqauresum(n) : 
+def squaresum(n):
     return (n * (n + 1) * (2 * n + 1)) // 6
-n = int(input("Enter the value: " ))
+
+
+n = int(input("Enter the value: "))
 print(squaresum(n))
