90373b86ff3423e530bcc8dc0c6b5ead03f37151
Sanskar Shrivastava <76647896+SanskarSh@users.noreply.github.com>
2022-10-24 12:59:23 +0530

    Revert "Adding code"
    This reverts commit 089004968859f3de1b828f0c5e5a856514dbb84f.

--- Patterns/Symbol_Patterns/code/Symbol_Pattern_115/symbol_pattern_115.py
+++
@@ -1,7 +0,0 @@
-for row in range(1, 6):
-    for col in range(1, 10):
-        if (row == 5) or (row + col == 6) or (col - row == 4):
-            print("*", end="")
-        else:
-            print(end=" ")
-    print()
