f4fca4a5b30b25cd922a897da860a6b195e5f9d0
Sanskar Shrivastava <76647896+SanskarSh@users.noreply.github.com>
2022-10-24 13:00:56 +0530

    Revert "Adding symbol pattern 118"

--- Patterns/Symbol_Patterns/code/Symbol_Pattern_118/symbol_pattern_118.py
+++
@@ -1,7 +0,0 @@
-for row in range(1, 6):
-    for col in range(1, 10):
-        if (row == 1) or (row + col == 10) or (col - row == 0):
-            print("*", end="")
-        else:
-            print(end=" ")
-    print()
