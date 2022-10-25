7b06271ebc0a438227e3275714be982b29956f3a
jahnwi2302 <jahnwi23@gmail.com>
2022-10-25 23:22:10 +0530

    Adding code to find area of rectangle

---
+++ Math/area_of_rectangle.py
@@ -0,0 +1,9 @@
+# Taking Length And Breadth Inputs
+length = float(input('Enter The Length Of The Rectangle: '))
+breadth = float(input('Enter The Breadth Of The Trianlge: '))
+
+# Calculating The Area Of The Rectangle
+area = (length * breadth)
+
+# Ouputing The Area
+print('The Area Of The Rectangle Is:', area)
