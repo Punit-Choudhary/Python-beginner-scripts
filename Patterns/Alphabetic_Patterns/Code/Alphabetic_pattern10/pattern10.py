cfdeaef3fd7ac98d2378b52c641a3a977df356d6
Shubham270301 <shubham27.sharma03@gmail.com>
2022-10-28 19:29:31 +0530

    Updated pattern10.py
    Linted the code using flake8

--- Patterns/Alphabetic_Patterns/Code/Alphabetic_pattern10/pattern10.py
+++ Patterns/Alphabetic_Patterns/Code/Alphabetic_pattern10/pattern10.py
@@ -1,11 +1,12 @@
-#Code for Alphabetic Pattern10
+# Code for Alphabetic Pattern10
 def letter_range(start, end):
-  	# outer loop
+    # outerloop
     for i in range(start, end):
         # inner loop
         for j in range(65, i + 1):
             print(f"{chr(j)}", end="")
         print()
+
 
 # calling Function
 letter_range(65, 70)
