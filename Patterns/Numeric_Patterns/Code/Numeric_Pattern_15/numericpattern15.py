303dd03eb4a59bab8f43b30a3ddcedd7489f170f
akshatjain24 <akshatjainmail@gmail.com>
2022-10-30 12:23:03 +0530

    lint test pass

--- Patterns/Numeric_Patterns/Code/Numeric_Pattern_15/numericpattern15.py
+++ Patterns/Numeric_Patterns/Code/Numeric_Pattern_15/numericpattern15.py
@@ -1,5 +1,5 @@
 x = int(input())
-for y in range(1,x+1):
-    for i in range(y,y+x):
-        print(i,end=' ')
+for y in range(1, x+1):
+    for i in range(y, y+x):
+        print(i, end=' ')
     print('\n')
