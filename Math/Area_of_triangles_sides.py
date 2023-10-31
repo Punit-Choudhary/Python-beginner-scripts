e0d5f93dcd9f4cfa6f591aae2233b0c044ffd13c
Mr.B <114798102+MrB141107@users.noreply.github.com>
2023-10-31 12:54:13 +0530

    Update Area_of_triangles_sides.py

--- Math/Area_of_triangles_sides.py
+++ Math/Area_of_triangles_sides.py
@@ -5,7 +5,8 @@ s1 = float(input("Enter the value of A :"))
 s2 = float(input("Enter the value of B :"))
 s3 = float(input("Enter the value of C :"))
 
-# Defining the formula of Heron's to calculate the area of triangle.
+
+# Defining the formula of Heron's to calculate the area of a triangle.
 def Area(a, b, c):
     sp = (a + b + c) / 2
     area = (sp) * (sp - a) * (sp - b) * (sp - c)
@@ -13,5 +14,5 @@ def Area(a, b, c):
     print(f"{f_area}" + " square units")
 
 
-# Printing the area of triangle.
+# Printing the area of a triangle.
 Area(s1, s2, s3)
