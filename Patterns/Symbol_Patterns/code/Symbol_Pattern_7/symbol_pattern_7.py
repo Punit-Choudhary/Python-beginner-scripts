a312b0c05847868fc5dbf52d02623f19b98794e5
Punit Choudhary <65050160+Punit-Choudhary@users.noreply.github.com>
2024-10-25 01:10:07 +0530

    Fix linting issues

--- Patterns/Symbol_Patterns/code/Symbol_Pattern_7/symbol_pattern_7.py
+++ Patterns/Symbol_Patterns/code/Symbol_Pattern_7/symbol_pattern_7.py
@@ -2,8 +2,9 @@ def print_pattern():
     rows = 5  # Number of rows in the pattern
     for i in range(rows, 0, -1):
         # Print leading spaces to align the stars
-        print(' ' * (rows - i), end='')
+        print(" " * (rows - i), end="")
         # Print stars for the current row
-        print('* ' * i)
+        print("* " * i)
+
 
 print_pattern()
